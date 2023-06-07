1a449857557fe4f907bb9c255b323714d4264095
Gustavo Henrique de Moraes <104281604+GustavoMraes@users.noreply.github.com>
2023-06-07 11:33:52 -0300

    Todos os arquivos corrigidos e finalizados do curso!

---
+++ Módulo3/Parte1/Mario.py
@@ -0,0 +1,8 @@
+from kivy.app import App
+from kivy.uix.image import Image, AsyncImage
+
+class MarioApp(App):
+    def build(self):
+        img = AsyncImage(source='https://supermariorun.com/assets/img/stage/mario03.png',size_hint=(1, .5),pos_hint={'center_x': .5, 'center_y': .5})
+        return img
+
---
+++ Módulo3/Parte1/teste.py
@@ -0,0 +1,21 @@
+from Mario import MarioApp
+from MainApp import MainApp
+from TesteCores import HBoxLayoutExample
+from Calculadora import Calculadora
+
+if __name__ == '__main__':
+   #Teste MainApp
+   #app = MainApp()
+   #app.run()
+   
+   #Teste Mario
+   #app = MarioApp()
+   #app.run()  
+   
+    #Teste Cores
+    #app = HBoxLayoutExample()
+    #app.run()
+
+   #Teste Calculadora
+   app = Calculadora()
+   app.run()
