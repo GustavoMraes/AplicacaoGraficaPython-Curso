64c420dfc53fa50b17ae3dbc3137d6c302b223db
Gustavo Henrique de Moraes <104281604+GustavoMraes@users.noreply.github.com>
2023-06-14 09:13:47 -0300

    Deletando arquivos repetidos

--- CursoPythonBradesco/Módulo2/Parte1 - Esfera/teste.py
+++
@@ -1,9 +0,0 @@
-from Esfera import Esfera
-
-
-bola1 = Esfera('vermelha',4)
-bola2 = Esfera('azul',7)
-print(f'O volume da bola 1 é{bola1.volume()}cm^3')
-print(f'A área superﬁcial da bola 1 é{bola1.area()}cm^2')
-print(bola1.volume())
-print(Esfera.volume(bola1))
